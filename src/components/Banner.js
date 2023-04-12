9710b4731f86f525f20f6cde34c3432ce1badb50
Vishvas Patel <pvishvas7@gmail.com>
2023-04-12 23:39:03 +0530

    basic structure completed

---
+++ src/components/Banner.js
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "a Web Developer", "an Otaku", "an Artist" ];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hello! I'm Vishvas`}<br></br> {`I am `}<span className="txt-rotate" dataPeriod="1000" data-rotate='[ "a Web Developer", "an Otaku", " an Artist" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>I'm passionate about creating and designing web applications, and I thrive on bringing innovative ideas to life in the digital world. When I'm not coding, you can often find me indulging in my hobbies. I'm a big anime fan and love immersing myself in the captivating stories and vibrant characters that anime has to offer.<br></br><br></br>Additionally, I have a soft spot for cats and enjoy spending time with these furry creatures. With my skills in web development and my interests, I have a unique blend of professional expertise and personal passions that make me a well-rounded individual.</p>
+                  <button onClick={() => window.location.href = 'https://www.linkedin.com/in/vishvas-patel-6834781a0/'}>Connect with me on LinkedIn!<ArrowRightCircle size={25} /></button>
+              </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import { Container, Row, Col } from "react-bootstrap";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} sm={6}>
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/vishvas-patel-6834781a0/"><img src={navIcon1}  alt="Icon" /></a>
+              {/* <a href="#"><img src={navIcon3} alt="Icon" /></a> */}
+            </div>
+            {/* <p>Copyright 2023. All Rights Reserved</p> */}
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
